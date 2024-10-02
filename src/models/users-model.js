4f00a29c69a1860b429bab1dba6b038a091a0bfa
Gabriel Vignoli <vignoligabriel052@gmail.com>
2024-10-02 20:37:27 -0300

    Criação do setup do projeto, utilizando as bibliotecas express, jsonwebtoken, dotenv, bcrypt e uuid para malhorar a segurança

---
+++ src/models/users-model.js
@@ -0,0 +1,33 @@
+// biblioteca para a criação de IDs únicos
+const uuid = require('uuid').v4
+// biblioteca para criptografar senhas
+const bcrypt = require('bcrypt')
+
+const users = [
+  {
+    id: "1",
+    name: "Gabriel Vignoli",
+    email: "gabriel@email.com",
+    password: "123456",
+  },
+  { id: "2", name: "Miguel Vignoli", email: "gabriel@email.com", password: "0000" },
+];
+
+module.exports = {
+    getAllUsers: () => users,
+  
+    getUserById: (id) => users.find(user => user.id === id),
+  
+    getUserByEmail: (email) => users.find(user => user.email === email),
+  
+    createUser: (name, email, password) => {
+      const newUser = {
+        id: uuid(),
+        name,
+        email,
+        password: bcrypt.hashSync(password, 10)
+      }
+      users.push(newUser)
+      return newUser
+    }
+}
